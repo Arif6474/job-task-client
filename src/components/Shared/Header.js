0a0f38e6143a2e4a5655f27cdf22bfc5d7beafa2
Arif6474 <arifulislam64743@gmail.com>
2022-06-30 21:06:20 +0600

    design to do form and implement react hook form , enter key

--- src/components/Shared/Header.js
+++ src/components/Shared/Header.js
@@ -1,27 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div>
-            <div class="navbar bg-base-100">
-  <div class="navbar-start">
-    <div class="dropdown">
-      <label tabindex="0" class="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+            <div className="navbar  bg-accent text-white font-bold">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <label tabindex="0" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
-      <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Item 1</a></li>
-        <li><a>Item 2</a></li>
-        <li><a>Item 3</a></li>
+      <ul tabindex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-accent text-white font-bold rounded-box w-52">
+      
+      <li><Link to='/completed-task'>Completed Task</Link></li>
+      <li><Link to='/to-do'>To Do</Link></li>
+      <li><Link to='/calendar'>Calender</Link></li>
       </ul>
     </div>
-    <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
+    <Link to="/" className="btn btn-ghost normal-case text-2xl font-bold">EndGame</Link>
   </div>
-  <div class="navbar-center hidden lg:flex">
-    <ul class="menu menu-horizontal p-0">
-      <li><a>Item 1</a></li>
-      <li><a>Item 2</a></li>
-      <li><a>Item 3</a></li>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal p-0">
+    
+      <li><Link to='/completed-task'>Completed Task</Link></li>
+      <li><Link to='/to-do'>To Do</Link></li>
+      <li><Link to='/calendar'>Calender</Link></li>
     </ul>
   </div>
   
--- src/components/ToDo/ToDo.js
+++ src/components/ToDo/ToDo.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ToDo = () => {
+    const [task, setTask] = useState('');
+   
+  
+    const handleSubmit = event => {
+      event.preventDefault();
+      const task = event.target.task.value
+  
+      console.log(task);
+      setTask('')
+    };
+    
+  
     return (
-        <div>
-            <h1>this is a to do page</h1>
+        <div className='flex h-screen justify-center items-center'>
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <div className="card-body">
+                    <h2 className="text-center text-2xl font-bold text-lime-600">To-Do</h2>
+                    <form onSubmit={handleSubmit}>
+                        <div className="form-control w-full max-w-xs">
+                            <input type="text"  name="task"
+            value={task}
+            onChange={event => setTask(event.target.value)} placeholder="Daily Task" class="input input-bordered input-primary w-full max-w-xs" />
+                        </div>
+                        <input className='my-4 w-full max-w-xs btn btn-accent border-0 text-white' type="submit" value="submit" />
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
