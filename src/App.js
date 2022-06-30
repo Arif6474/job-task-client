5ee38b180b6fe1c796d19634d8b1f74cec38dffc
Arif6474 <arifulislam64743@gmail.com>
2022-06-30 17:40:29 +0600

    added header and footer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import { Route, Routes } from 'react-router-dom';
+import CompletedTask from './components/CompletedTask/CompletedTask';
+import Home from './components/Home/Home';
+import Footer from './components/Shared/Footer';
+
+import Header from './components/Shared/Header';
+import ToDo from './components/ToDo/ToDo';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Header></Header>
+    <Routes>
+      <Route path="/" element={<Home></Home>}></Route>
+      <Route path="home" element={<Home></Home>}></Route>
+      <Route path="completed-task" element={<CompletedTask></CompletedTask>}></Route>
+      <Route path="to-do" element={<ToDo></ToDo>}></Route>
+      <Route path="calendar" element={<ToDo></ToDo>}></Route>
+    </Routes>
+    <Footer></Footer>
     </div>
   );
 }
