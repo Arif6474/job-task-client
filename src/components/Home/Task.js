8c8c28a1609c41c5a988f973883c0d9f377696c4
Arif6474 <arifulislam64743@gmail.com>
2022-07-01 02:45:32 +0600

    implement checkbox condition

--- src/components/Home/Task.js
+++ src/components/Home/Task.js
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Task = ({t}) => {
     const {task} = t;
+    const [isChecked, setIsChecked] = useState(false);
+    const handleOnChange = () => {
+        setIsChecked(!isChecked);
+      };
+    const completedTask = (data) =>{
+        
+        const completeTask = {data}
+        console.log(completeTask);
+
+        const url = `http://localhost:5000/tasks`
+        fetch(url,{
+            method: 'POST',
+            headers: {'content-type' : 'application/json'},
+            body: JSON.stringify(completeTask)
+        })
+        .then(res => res.json())
+        .then(result=> console.log(result))
+
+    };
     return (
         <tr>
         
         <td><div class="form-control">
   <label class="cursor-pointer label">
-    
-    <input type="checkbox"  class="checkbox checkbox-accent" />
-  </label>
+  {
+            isChecked ? <input type="checkbox" checked="checked" class="checkbox checkbox-accent" /> :
+        
+    <input type="checkbox"
+    checked={isChecked}
+    onChange={handleOnChange}
+      onClick={()=>completedTask(t.task)} class="checkbox checkbox-accent" />
+  }
+  </label> 
 </div></td>
         <td>{task}</td>
+       
         <td> <button  className="btn btn-outline btn-success btn-xs">Edit</button></td>
        
       </tr>
